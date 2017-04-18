2524828f26b3ad540a75fd807abeb6b237d552e1
Tomek <tomekdev@users.noreply.github.com>
2017-04-18 18:31:24 +0200

    Issue #150 - validate duplicate index numbers

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -166,7 +166,18 @@ class StudentController extends Controller
         $request['name'] = ucfirst(mb_strtolower($request['name']));
         $request['surname'] = ucfirst(mb_strtolower($request['surname']));
 
-        $student = $id ? Student::find($id) : null;
+        if ($id) {
+            $student = Student::find($id);
+        }
+        else {
+            $student = Student::where(['index' => $request['index']])->get();
+            if(count($student) > 0) {
+                Session::flash('error', 'Student o takim numerze indeksu już istnieje w bazie.');
+                $request->flash();
+                return redirect()->back();
+            }
+            $student = null;
+        }
 
         if($student) {
             $studies = $student->getDBStudies();
