6e7a288c88399fda50f8f23bfe9fcf14d0f0354a
Tomek <tomekdev@users.noreply.github.com>
2017-04-19 19:35:29 +0200

    Validate unique field name

--- app/Http/Controllers/Admin/FieldController.php
+++ app/Http/Controllers/Admin/FieldController.php
@@ -83,7 +83,18 @@ class FieldController extends Controller
             return redirect()->back()->withErrors($v->errors());
         }
 
-        $field = $id ? Field::find($id) : new Field();
+        if ($id) {
+            $field = Field::find($id);
+        }
+        else {
+            $field = Field::where(['name' => $request['name']])->get();
+            if(count($field) > 0) {
+                Session::flash('error', 'Kierunek o takiej nazwie już istnieje w bazie.');
+                $request->flash();
+                return redirect()->back();
+            }
+            $field = new Field();
+        }
         $field->fill($request->all());
         $field->save();
         Session::flash('success', 'Kierunek został pomyślnie zapisany.');
