6fcfdfb3d4b9d6f641115e82f7dc6c511dcb5de2
Patryk Trompeta <MrBlue2583@gmail.com>
2017-04-24 22:44:39 +0200

    fix for issue made by myself (again?)

--- app/Http/Controllers/Admin/StudyFormController.php
+++ app/Http/Controllers/Admin/StudyFormController.php
@@ -7,7 +7,6 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use App\StudyForm;
 use \Session;
-use Illuminate\Support\Facades\Validator;
 
 class StudyFormController extends Controller
 {
