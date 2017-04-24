e72056fc0b34a932ee7481c270c7ff6a073e39b8
Patryk Trompeta <MrBlue2583@gmail.com>
2017-04-24 22:43:05 +0200

    fixes issue made by myself (duplicate Validator facade)

--- app/Http/Controllers/Admin/DegreeController.php
+++ app/Http/Controllers/Admin/DegreeController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Validator;
 use \Session;
-use Illuminate\Support\Facades\Validator;
 
 class DegreeController extends Controller
 {
